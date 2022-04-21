b6eb4d662b19e2576dac8a5a2761a08df9faed9d
klina8575 <klina8575@gmail.com>
2022-04-21 18:46:00 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import MyComponent from './components/MyComponent';
 // import MyComponentFunc from './components/MyComponentFunc';
 // import Counter from './components/Counter';
-import Says from './components/Says';
+// import Says from './components/Says';
+import EventPractice from './components/22-04-21/EventPractice';
 function App() {
-  return <Says />;
+  return <EventPractice />;
 }
 
 export default App;
