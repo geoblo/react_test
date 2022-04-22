6ae53894e2238f4358cfea1d2f44c73008745394
klina8575 <klina8575@gmail.com>
2022-04-22 15:47:17 +0900

    up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@
 // import Says from './components/Says';
 // import EventPractice from './components/22-04-21/EventPractice';
 // import EventPracticeFunc from './components/22-04-21/EventPracticeFunc';
-import ValidationSample from './components/22-04-22/ValidationSample';
+// import ValidationSample from './components/22-04-22/ValidationSample';
+import ScrollBox from './components/22-04-22/ScrollBox';
 function App() {
-  return <ValidationSample />;
+  return (
+    <div>
+      <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+    </div>
+  );
 }
 
 export default App;
