230cc689cb802e7877ddcc264737e58cfbd60a93
klina8575 <klina8575@gmail.com>
2022-04-22 18:00:38 +0900

    up

--- src/App.js
+++ src/App.js
@@ -6,9 +6,33 @@
 // import EventPracticeFunc from './components/22-04-21/EventPracticeFunc';
 // import ValidationSample from './components/22-04-22/ValidationSample';
 // import ScrollBox from './components/22-04-22/ScrollBox';
-import IterationSample from './components/22-04-22/IterationSample';
-function App() {
-  return <IterationSample></IterationSample>;
+// import IterationSample from './components/22-04-22/IterationSample';
+import LifeCycleSample from './components/22-04-22/LifeCycleSample';
+import { Component } from 'react';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}></LifeCycleSample>
+      </div>
+    );
+  }
 }
 
 export default App;
