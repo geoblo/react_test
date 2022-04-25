e143e096fb358d4496b560ad4df23693bf0586fc
klina8575 <klina8575@gmail.com>
2022-04-25 15:00:18 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-// import MyComponent from './components/MyComponent';
-// import MyComponentFunc from './components/MyComponentFunc';
-// import Counter from './components/Counter';
-// import Says from './components/Says';
-// import EventPractice from './components/22-04-21/EventPractice';
-// import EventPracticeFunc from './components/22-04-21/EventPracticeFunc';
-// import ValidationSample from './components/22-04-22/ValidationSample';
-// import ScrollBox from './components/22-04-22/ScrollBox';
-// import IterationSample from './components/22-04-22/IterationSample';
-import LifeCycleSample from './components/22-04-22/LifeCycleSample';
-import ErrorBoundary from './components/22-04-22/ErrorBoundary';
-import { Component } from 'react';
+import React from 'react';
+import Info from './components/22-04-25/Info';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: '#000000',
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
+function App(props) {
+  return <Info />;
 }
 
 export default App;
